2e68ba4734349e5f4b8cb4d062a06f1fdd23f114
Gabriel2980 <142542904+Gabriel2980@users.noreply.github.com>
2025-09-10 03:20:22 -0300

    Add files via upload

--- Atv3.py
+++ Atv3.py
@@ -1,86 +1,67 @@
 import math
 
-def criarADF(estados, estadosFuturos,estadoInicial, estadosAceita, alfabeto, transferencia):
-    estados
+def criarADF(estados):
 
-def AFD(estado, simboloNovo):
-    if(simboloNovo != '0' and simboloNovo != '1'):
-        return estado
-    if(estado == 'q1'):
-        if(simboloNovo == '0'):
-            estado = 'q1'
+    def ADF(simboloNovo):
+        estadoAtual = estados[1][0]
+        for simbolo in simboloNovo:
+            for i in range(len(estados[0])):
+                if(estados[0][i] == estadoAtual):
+                    break
+            for j in range(len(estados[3])):
+                if(estados[3][j] == simbolo):
+                    break
+            estadoAtual = estados[i+4][j+1]
+        if(estadoAtual in estados[2]):
+            resultado = 'aceita'
         else:
-            estado = 'q2'
-    elif(estado == 'q2'):
-        if(simboloNovo == '0'):
-            estado = 'q3'
-        else:
-            estado = 'q2'
-    elif(estado == 'q3'):
-        estado = 'q2'
-    return estado
+            resultado = 'rejeita'
+        return resultado
+    return ADF
 
-#Guardando todo o input do site para criar o automato
-automato = input()
+def adicionarEstado(novaInformacao, etapaAtualIndex):
+    if(len(estadosADF) <= etapaAtualIndex):
+        estadosADF.append([])
+    estadosADF[etapaAtualIndex].append(novaInformacao)
+    return estadosADF
 
 #criando os índex para as etapas de criação da ADF 
-estadosIndex=0
 etapaAtualIndex = 0
-estadosAceitacaoIndex=0
-alfabetoIndex = 0
-transferenciaIndexAlfabeto = 0
-transferenciaIndexEstados = 0
 transferenciaIndex = 0
 
 novaInformacao = None
 
+#criando matriz para guardar os estados, estados de aceitação, alfabeto e transferência
+estadosADF = []
+
 #criando todas etapas da ADF
 etapaDeCriacaoDaADF = ['CriandoEstados', 'EstadoInicial', 'EstadosDeAceitacao', 'CriandoAlfabeto', 'Transferencia']
-etapaAtual = etapaDeCriacaoDaADF[etapaAtualIndex]
 
-for letraNova in automato: 
+while(etapaAtualIndex <= len(etapaDeCriacaoDaADF)-1+(len(estadosADF[0]) if len(estadosADF) > 0 else 0)):
 
-    #adiciona uma letra na string novaInformacao até encontrar um espaço
-    if(letraNova != ' '):
-        if(letraNova != '\n')
+    #Guardando todo o input do site para criar o automato  
+    automato = input()
+    for letraNova in automato: 
+
+        #adiciona uma letra na string novaInformacao até encontrar um espaço ou \n
+        if(letraNova != ' '):
             if(novaInformacao == None):
                 novaInformacao = letraNova
             else:
                 novaInformacao += letraNova
-        
-        #quando encontrar um \n passa para a próxima etapa até finalizar todas etapas
-        elif(etapaAtualIndex < len(etapaDeCriacaoDaADF)):
-            etapaAtualIndex += 1
-            etapaAtual = etapaDeCriacaoDaADF[etapaAtualIndex]
-        #ao finalizar todas etapas para criar a ADF verifica se os símbolos são aceitos ou rejeitados
-        elif(len(transferencia) >= len(estados)):
-            resultado = AFD(novaInformacao)
-
-    #adiciona a novaInformacao nas viriáveis dependendo da etapa atual
-    else:
-        if(etapaAtual == 'CriandoEstados'):
-            #guarda os estados aumentando o index até passar para a próxima etapa
-            estados[estadosIndex] = novaInformacao
-            estadosIndex += 1
-
-        elif(etapaAtual == 'EstadoInicial'):
-            #guarda o estado inicial
-            estadoInicial = novaInformacao
-
-        elif(etapaAtual == 'EstadosDeAceitacao'):
-            #guarda estados de aceitação aumentando o index até passar para a próxima etapa
-            estadosAceitacao[estadosAceitacaoIndex] = novaInformacao
-            estadosAceitacaoIndex += 1
-
-        elif(etapaAtual == 'CriandoAlfabeto'):
-            #guarda o alfabeto aumentando o index até passar para a próxima etapa
-            alfabeto[alfabetoIndex] = novaInformacao
-            alfabetoIndex += 1
-        
-        #transferencia
+        #adiciona a novaInformacao nas viriáveis dependendo da etapa atual
         else:
-            if(transferenciaIndexAlfabeto < alfabetoIndex):
-                transferencia[transferenciaIndexEstados][transferenciaIndexAlfabeto] = novaInformacao
-                transferenciaIndexAlfabeto += 1
-            elif(transferenciaIndexEstados < estadosIndex):
-                transferenciaIndexEstados += 1
+            estadosADF= adicionarEstado(novaInformacao, etapaAtualIndex)
+            novaInformacao = None
+
+            
+    #quando encontrar um \n passa para a próxima etapa até a transferência
+    estadosADF = adicionarEstado(novaInformacao, etapaAtualIndex)
+    novaInformacao = None
+    etapaAtualIndex += 1
+
+ADF = criarADF(estadosADF)
+for verificacao in range(len(estadosADF[-1])): 
+        resultado = ADF(estadosADF[-1][verificacao])
+        print(resultado)
+        novaInformacao = None
