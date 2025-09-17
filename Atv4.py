0a99464f565430923a6e50bc0bdd7bfc20236b10
Gabriel2980 <142542904+Gabriel2980@users.noreply.github.com>
2025-09-17 04:43:57 -0300

    Add files via upload

--- Atv4.py
+++ Atv4.py
@@ -1,52 +1,59 @@
-import math
-import time
+#Testa se mais estados devem entrar junto do estado novo
+def entrandoEmEstado(estados, estados_lista, estadosPossiveis):
 
-def criarADF(estados):
+    estadosExtras = 0
+    adicionarEstadosExtras = []
+    if(estados[4+int(estados_lista[-1])-int(estados[4][0][-1])][-1] != 'vazio'):
+                        estadosExtras += 1
+                        adicionarEstadosExtras.append(int(estados_lista[-1]))
+    for todosCasos in range(estadosExtras):  
+        estadosPossiveis.append(estados[4+int(estados_lista[-1])-int(estados[4][0][-1])][-1][todosCasos*3:2+todosCasos*3])
+    
+    return estadosPossiveis
 
-    def ADF(simboloNovo):
-        estadosPossiveis = []
-        estadosFuturos = 0
-        estadosPossiveis.append(estados[2][0])
-        for simbolo in simboloNovo:
-            copiaEstadosPossiveis = estadosPossiveis
-            for estadoAtual in copiaEstadosPossiveis:
-                print("Estado autal:", estadoAtual)
-                print("Simbolo:", simbolo)
+#cria a ADF
+def ADF(simboloNovo, estados):
+    estadosPossiveis = []
+    estadosPossiveis.append(estados[2][0])
+    estadosPossiveis = entrandoEmEstado(estados, estados[2][0], estadosPossiveis)
+    #Verifica todos os simbolos da palavra
+    for simbolo in simboloNovo:
+        #copia os estadosPossiveis para ela não alterar durante o loop
+        copiaEstadosPossiveis = estadosPossiveis.copy()
+        print(estadosPossiveis)
+        print(simbolo)
+        #verifica os simbolos em cada estado possível
+        for estadoAtual in copiaEstadosPossiveis:
+            estadosFuturos = 0
+            #encontra o index do estado atual e do simbolo atual
+            for i in range(len(estados[0])):
+                if(estados[0][i] == estadoAtual):
+                    break
+            for j in range(len(estados[1])):
+                if(estados[1][j] == simbolo):
+                    break
+            #verifica se existem mais de 1 estado possível
+            for verificar in estados[i+4][j+1]:
+                if(verificar == ','):
+                    estadosFuturos += 1
+            #adiciona todos estados na lista de estadosPossiveis        
+            for todosCasos in range(estadosFuturos+1):
+                if(estados[i+4][j+1] != 'vazio'):
+                    estados_lista = estados[i+4][j+1].split(',')
+                    estadosPossiveis.append(estados_lista[todosCasos])
+                    estadosPossiveis = entrandoEmEstado(estados, estados_lista[todosCasos], estadosPossiveis)
+            #remove o estado que foi estudado e atualizado
+            estadosPossiveis.pop(0)
+    
+    print(estadosPossiveis)
+    resultado = 'rejeita'
+    #se o estado aceito for um dos estadosPossiveis, a palavra é aceita
+    for estadoAtual in estadosPossiveis:
+        if(estadoAtual in estados[3]):
+            resultado = 'aceita'
+    return resultado
 
-                for i in range(len(estados[0])):
-                    print("Estado Teste i:", estados[0][i])
-                    if(estados[0][i] == estadoAtual):
-                        break
-                for j in range(len(estados[1])):
-                    print("Estado Teste j:", estados[1][j])
-                    if(estados[1][j] == simbolo):
-                        break
-                print("i: %d J: %d" %(i, j))
-                
-                for verificar in estados[i+4][j+1]:
-                    if(verificar == ','):
-                        estadosFuturos += 1
-                print(estadosFuturos)
-                time.sleep(3)
-                for todosCasos in range(estadosFuturos+1):
-                    if(estados[i+4][j+1] != 'vazio'):
-                        print(estados[i+4][j+1])
-                        print(i)
-                        print(j)
-                        estadosPossiveis.append(estados[i+4][j+1][todosCasos*3:2+todosCasos*3])
-                print(estadosPossiveis)
-                estadosPossiveis.pop(0)
-                print(estadosPossiveis)
-
-            time.sleep(3)
-        print("Aqui")
-        resultado = 'rejeita'
-        for estadoAtual in estadosPossiveis:
-            if(estadoAtual in estados[2]):
-                resultado = 'aceita'
-        return resultado
-    return ADF
-
+#adiciona estados para criar a ADF
 def adicionarEstado(novaInformacao, etapaAtualIndex):
     if(len(estadosADF) <= etapaAtualIndex):
         estadosADF.append([])
@@ -86,8 +93,7 @@ while(etapaAtualIndex <= len(etapaDeCriacaoDaADF)-1+(len(estadosADF[0]) if len(e
     novaInformacao = None
     etapaAtualIndex += 1
 
-ADF = criarADF(estadosADF)
-
+#verifica todas palavras    
 for verificacao in range(len(estadosADF[-1])): 
-        resultado = ADF(estadosADF[-1][verificacao])
+        resultado = ADF(estadosADF[-1][verificacao], estadosADF)
         print(resultado)
